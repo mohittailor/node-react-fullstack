f09a3a33fbc07029364a350313e7a3d9285e0faa
mohittailor <lanetteam.mohitt@gmail.com>
2017-08-03 15:08:38 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	googleClientID: '543362620019-heiv3jd9on6u7t39s79kojks4jshvi0j.apps.googleusercontent.com'
+	,googleClientSecret: 'Eyy-OqxONvU_eyX-rInKDUWw'
+	,mongoURI: 'mongodb://mohittailor:lanetteam1@ds129043.mlab.com:29043/emaily-app'
+	,cookieKey: 'fefuhbfhwfhurbgfhvbrgfvbrgfrvbrvfvwrbnsadjkw'
+};
