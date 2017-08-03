d1a7dcc7c8db1c8aa79f5eef3f31dc435bcf4f5b
mohittailor <lanetteam.mohitt@gmail.com>
2017-08-03 14:08:25 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+	googleClientID: '543362620019-heiv3jd9on6u7t39s79kojks4jshvi0j.apps.googleusercontent.com'
+	,googleClientSecret: 'Eyy-OqxONvU_eyX-rInKDUWw'
+	,mongoURI: 'mongodb://mohittailor:lanetteam2@ds129043.mlab.com:29043/emaily-app'
+	,cookieKey: 'fefuhbfhwfhurbgfhvbrgfvbrgfrvbrvfvwrbnsadjkw'
+};
--- config/key.js
+++ config/key.js
@@ -1,6 +1,8 @@
-module.exports = {
-	googleClientID: '543362620019-heiv3jd9on6u7t39s79kojks4jshvi0j.apps.googleusercontent.com'
-	,googleClientSecret: 'Eyy-OqxONvU_eyX-rInKDUWw'
-	,mongoURI: 'mongodb://mohittailor:lanetteam1@ds129043.mlab.com:29043/emaily-app'
-	,cookieKey: 'fefuhbfhwfhurbgfhvbrgfvbrgfrvbrvfvwrbnsadjkw'
-};
+
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys!!!
+  module.exports = require('./dev');
+}
